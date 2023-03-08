f45bd7717b3afc7114c6a3592cb178daba20f986
JoshuaMakuch <joshuamakuch9@gmail.com>
2023-03-08 08:18:56 -0700

    Add project files.

---
+++ DinerMenu/DinerMenuForm.Designer.vb
@@ -0,0 +1,90 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class DinerMenuForm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.ExitButton = New System.Windows.Forms.Button()
+        Me.ToolTip1 = New System.Windows.Forms.ToolTip(Me.components)
+        Me.FishButton = New System.Windows.Forms.Button()
+        Me.SaladButton = New System.Windows.Forms.Button()
+        Me.SoupButton = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'ExitButton
+        '
+        Me.ExitButton.Location = New System.Drawing.Point(743, 441)
+        Me.ExitButton.Name = "ExitButton"
+        Me.ExitButton.Size = New System.Drawing.Size(116, 64)
+        Me.ExitButton.TabIndex = 0
+        Me.ExitButton.Text = "Exit"
+        Me.ToolTip1.SetToolTip(Me.ExitButton, "Exit Button")
+        Me.ExitButton.UseVisualStyleBackColor = True
+        '
+        'FishButton
+        '
+        Me.FishButton.Location = New System.Drawing.Point(256, 441)
+        Me.FishButton.Name = "FishButton"
+        Me.FishButton.Size = New System.Drawing.Size(116, 64)
+        Me.FishButton.TabIndex = 2
+        Me.FishButton.Text = "Fish"
+        Me.FishButton.UseVisualStyleBackColor = True
+        '
+        'SaladButton
+        '
+        Me.SaladButton.Location = New System.Drawing.Point(134, 441)
+        Me.SaladButton.Name = "SaladButton"
+        Me.SaladButton.Size = New System.Drawing.Size(116, 64)
+        Me.SaladButton.TabIndex = 3
+        Me.SaladButton.Text = "Salad"
+        Me.SaladButton.UseVisualStyleBackColor = True
+        '
+        'SoupButton
+        '
+        Me.SoupButton.Location = New System.Drawing.Point(12, 441)
+        Me.SoupButton.Name = "SoupButton"
+        Me.SoupButton.Size = New System.Drawing.Size(116, 64)
+        Me.SoupButton.TabIndex = 4
+        Me.SoupButton.Text = "Soup"
+        Me.SoupButton.UseVisualStyleBackColor = True
+        '
+        'DinerMenuForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(871, 517)
+        Me.Controls.Add(Me.SoupButton)
+        Me.Controls.Add(Me.SaladButton)
+        Me.Controls.Add(Me.FishButton)
+        Me.Controls.Add(Me.ExitButton)
+        Me.Name = "DinerMenuForm"
+        Me.Text = "DinerMenuForm"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents ExitButton As Button
+    Friend WithEvents ToolTip1 As ToolTip
+    Friend WithEvents FishButton As Button
+    Friend WithEvents SaladButton As Button
+    Friend WithEvents SoupButton As Button
+End Class
---
+++ DinerMenu/DinerMenuForm.vb
@@ -0,0 +1,15 @@
+﻿'Joshua Makuch
+'RCET 0265
+'Spring 2023
+'Diner Menu Program
+
+Public Class DinerMenuForm
+
+
+
+    'Exit Button
+    Private Sub ExitButton_Click(sender As Object, e As EventArgs) Handles ExitButton.Click
+        Me.Close()
+    End Sub
+
+End Class
